74f1c819f26fef225069dcf3c412cbaa0e89498e
Lindemberg <lindembergparaiba@gmail.com>
2025-01-19 16:05:09 -0300

    feat: modal quase finalizado

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,6 +1,7 @@
 import styles from "./Modal.module.css";
 import xIcon from "../../assets/icons/x_close.svg";
 import img from "../../assets/img/temp_img.svg";
+import pokeball from "../../assets/icons/pokeball.svg";
 export const Modal = () => {
   return (
     <div>
@@ -13,7 +14,34 @@ export const Modal = () => {
             </button>
           </header>
           <main className={styles.main}>
-            <img src={img} alt="" />
+            <img className={styles.pokemon_img} src={img} alt="" />
+            <div className={styles.middle_box}>
+              <span>1</span>
+              <span>2</span>
+            </div>
+            <div className={styles.info_boxes}>
+              <div className={styles.info_box}>
+                <strong>
+                  <img className={styles.img} src={pokeball} alt="" />
+                  info
+                </strong>
+                <span>lorem ipsum</span>
+              </div>
+              <div className={styles.info_box_with_border}>
+                <strong>
+                  <img className={styles.img} src={pokeball} alt="" />
+                  info
+                </strong>
+                <span>lorem ipsum</span>
+              </div>
+              <div className={styles.info_box}>
+                <strong>
+                  <img className={styles.img} src={pokeball} alt="" />
+                  info
+                </strong>
+                <span>lorem ipsum</span>
+              </div>
+            </div>
           </main>
         </div>
       </section>
