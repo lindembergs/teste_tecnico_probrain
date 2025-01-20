8f8c8b6c0c50386695fc7fd1b160fe4dcfac2924
Lindemberg <lindembergparaiba@gmail.com>
2025-01-19 21:25:32 -0300

    feat: cards dinamicos works

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,18 +1,102 @@
 import styles from "./Card.module.css";
-import pokemonImg from "../../assets/img/temp_img.svg";
 import typeImg from "../../assets/icons/type_img.svg";
+import { useEffect, useState } from "react";
+import { api } from "../../services/api";
+interface IAbility {
+  name: string;
+  text: string;
+  type: string;
+}
+interface IAttack {
+  cost: string[];
+  name: string;
+  text: string;
+  damage: string;
+  convertedEnergyCost: number;
+}
+interface IResistance {
+  type: string;
+  value: string;
+}
+interface IWeakness {
+  type: string;
+  value: string;
+}
+interface ISet {
+  id: string;
+  name: string;
+  series: string;
+  printedTotal: number;
+  total: number;
+  ptcgoCode: string;
+  releaseDate: string;
+  updatedAt: string;
+}
+// interface IQuery {
+//   name: string;
+//   value: string | number;
+// }
+
+interface Pokemon {
+  id: string;
+  name: string;
+  supertype: string;
+  subtypes: string[];
+  hp?: string;
+  types?: string[];
+  evolesFrom?: string;
+  evolvesTo?: string[];
+  rules?: string[];
+  abilities?: IAbility[];
+  attacks?: IAttack[];
+  weaknesses?: IWeakness[];
+  resistances?: IResistance[];
+  retreatCost?: string[];
+  convertedRetreatCost?: number;
+  set?: ISet;
+  number: string;
+  artist?: string;
+  rarity: string;
+  flavorText?: string;
+  nationalPokedexNumbers?: number[];
+  images: {
+    small: string;
+    large: string;
+  };
+}
 export const Card = () => {
+  const [data, setData] = useState<Pokemon[]>([]);
+
+  const getData = async () => {
+    try {
+      const response = await api.get("");
+      const data = response.data.data;
+      setData(data);
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
-    <div className={styles.card}>
-      <img src={pokemonImg} alt="Imagem do pokemon no card" />
-      <div className={styles.name_box}>
-        <span>Bubasaur</span>
-        <img src={typeImg} alt="" />
-      </div>
-      <div className={styles.type_box}>
-        <span>Tipo planta</span>
-        <span>Comum</span>
-      </div>
-    </div>
+    <>
+      {data.map((pokemon) => (
+        <div key={pokemon.id} className={styles.card}>
+          <img src={pokemon.images.large} alt="Imagem do pokemon no card" />
+          <div className={styles.name_box}>
+            <span>{pokemon.name || "Desconhecido"}</span>
+            <img src={typeImg} alt="Tipo" />
+          </div>
+          <div className={styles.type_box}>
+            <span>{pokemon.types || "Sem tipo"}</span>
+            <span>{pokemon.rarity || "Sem raridade"}</span>
+          </div>
+        </div>
+      ))}
+    </>
   );
 };
--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,8 +1,9 @@
 import { Card } from "../Card/Card";
 import { Modal } from "./../Modal/Modal";
+import styles from "./Main.module.css";
 export const Main = () => {
   return (
-    <main>
+    <main className={styles.main}>
       <Card></Card>
       <Modal></Modal>
     </main>
