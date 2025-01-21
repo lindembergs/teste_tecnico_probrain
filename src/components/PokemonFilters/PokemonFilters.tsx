6026c74749f513a29ac44ac09cdf3519fd40439e
Lindemberg <lindembergparaiba@gmail.com>
2025-01-21 19:34:02 -0300

    style: ajuste no posicionamento do header align center

--- src/components/PokemonFilters/PokemonFilters.tsx
+++ src/components/PokemonFilters/PokemonFilters.tsx
@@ -8,12 +8,14 @@ export const PokemonFilters = ({
   onTypeChange,
 }: PokemonFiltersProps) => (
   <div className={styles.filters}>
-    <input
-      type="text"
-      placeholder="Pesquisar por nome"
-      value={searchTerm}
-      onChange={(e) => onSearchChange(e.target.value)}
-    />
+    <div>
+      <input
+        type="text"
+        placeholder="Pesquisar por nome"
+        value={searchTerm}
+        onChange={(e) => onSearchChange(e.target.value)}
+      />
+    </div>
     <select value={selectedType} onChange={(e) => onTypeChange(e.target.value)}>
       <option value="">Todos os tipos</option>
       <option value="Fire">Fogo</option>
