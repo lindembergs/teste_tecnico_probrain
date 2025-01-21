c2e39e587fe096a2bd6932eba216e902ac86c8d3
Lindemberg <lindembergparaiba@gmail.com>
2025-01-21 20:31:51 -0300

    style: componente de filtro responsivo

--- src/components/PokemonFilters/PokemonFilters.tsx
+++ src/components/PokemonFilters/PokemonFilters.tsx
@@ -1,6 +1,9 @@
 import { PokemonFiltersProps } from "../../types/pokemon";
 import styles from "./PokemonFilters.module.css";
+// import searchIcon from "../../assets/icons/search.svg";
 
+import filterIcon from "../../assets/icons/filter.svg";
+import SearchIcon from "@mui/icons-material/Search";
 export const PokemonFilters = ({
   searchTerm,
   selectedType,
@@ -8,20 +11,30 @@ export const PokemonFilters = ({
   onTypeChange,
 }: PokemonFiltersProps) => (
   <div className={styles.filters}>
-    <div>
+    <div className={styles.search_box}>
       <input
         type="text"
         placeholder="Pesquisar por nome"
         value={searchTerm}
         onChange={(e) => onSearchChange(e.target.value)}
       />
+      <button>
+        <SearchIcon />
+      </button>
     </div>
-    <select value={selectedType} onChange={(e) => onTypeChange(e.target.value)}>
-      <option value="">Todos os tipos</option>
-      <option value="Fire">Fogo</option>
-      <option value="Water">Água</option>
-      <option value="Grass">Grama</option>
-      <option value="Lightning">Elétrico</option>
-    </select>
+    <div className={styles.filter_box}>
+      <img src={filterIcon} alt="icone da sessão de filtro" />
+      <strong>Filtrar por:</strong>
+      <select
+        value={selectedType}
+        onChange={(e) => onTypeChange(e.target.value)}
+      >
+        <option value="">Todos os tipos</option>
+        <option value="Fire">Fogo</option>
+        <option value="Water">Água</option>
+        <option value="Grass">Grama</option>
+        <option value="Lightning">Elétrico</option>
+      </select>
+    </div>
   </div>
 );
