b91c6d34c4b5cd8961281a9e0517ebdd8c35b23f
Lindemberg <lindembergparaiba@gmail.com>
2025-01-22 10:56:50 -0300

    feat: adicao dos tipos dinâmicos no filtro de pokemons

--- src/components/PokemonFilters/PokemonFilters.tsx
+++ src/components/PokemonFilters/PokemonFilters.tsx
@@ -1,39 +1,58 @@
+import { usePokemonData } from "../../hooks/usePokemonData";
 import { PokemonFiltersProps } from "../../types/pokemon";
 import styles from "./PokemonFilters.module.css";
 import FilterListIcon from "@mui/icons-material/FilterList";
-
 import SearchIcon from "@mui/icons-material/Search";
+
 export const PokemonFilters = ({
   searchTerm,
   selectedType,
   onSearchChange,
   onTypeChange,
-}: PokemonFiltersProps) => (
-  <div className={styles.filters}>
-    <div className={styles.search_box}>
-      <input
-        type="text"
-        placeholder="Pesquisar por nome"
-        value={searchTerm}
-        onChange={(e) => onSearchChange(e.target.value)}
-      />
-      <button>
-        <SearchIcon />
-      </button>
+}: PokemonFiltersProps) => {
+  const { data, isLoading, error } = usePokemonData();
+
+  if (isLoading) {
+    return <div>Carregando...</div>;
+  }
+
+  if (error) {
+    return <div>Ocorreu um erro ao carregar os dados.</div>;
+  }
+
+  // Obtenha uma lista única de tipos
+  const uniqueTypes = data
+    ? Array.from(new Set(data.flatMap((pokemon) => pokemon.types)))
+    : [];
+
+  return (
+    <div className={styles.filters}>
+      <div className={styles.search_box}>
+        <input
+          type="text"
+          placeholder="Pesquisar por nome"
+          value={searchTerm}
+          onChange={(e) => onSearchChange(e.target.value)}
+        />
+        <button>
+          <SearchIcon />
+        </button>
+      </div>
+      <div className={styles.filter_box}>
+        <FilterListIcon style={{ fontSize: "40px", color: "#d4d7de" }} />
+        <strong>Filtrar por:</strong>
+        <select
+          value={selectedType}
+          onChange={(e) => onTypeChange(e.target.value)}
+        >
+          <option value="">Todos os tipos</option>
+          {uniqueTypes.map((type) => (
+            <option key={type} value={type}>
+              {type}
+            </option>
+          ))}
+        </select>
+      </div>
     </div>
-    <div className={styles.filter_box}>
-      <FilterListIcon style={{ fontSize: "40px", color: "#d4d7de" }} />
-      <strong>Filtrar por:</strong>
-      <select
-        value={selectedType}
-        onChange={(e) => onTypeChange(e.target.value)}
-      >
-        <option value="">Todos os tipos</option>
-        <option value="Fire">Fogo</option>
-        <option value="Water">Água</option>
-        <option value="Grass">Grama</option>
-        <option value="Lightning">Elétrico</option>
-      </select>
-    </div>
-  </div>
-);
+  );
+};
