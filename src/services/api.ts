b4279fbe4c1a4dbcea808ffa95bbea69d1f53060
Lindemberg <lindembergparaiba@gmail.com>
2025-01-22 16:08:45 -0300

    alt na api key

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-const apiKey = import.meta.env.VITE_API_KEY;
+const apiKey = import.meta.env.VITE_CREDENTIAL_TOKEN;
 export const api = axios.create({
   baseURL: "https://api.pokemontcg.io/v2/cards/",
   headers: {
